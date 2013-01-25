de1fe15b8f555efda78aa3c764de23d349259bb8
Matt Scheirer <matt.scheirer@gmail.com>
2013-01-25 12:57:08 +0000

    end of lesson 1

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
